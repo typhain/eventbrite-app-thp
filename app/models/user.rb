f82b7e2d6d05d1bc6d814cb1a18efc44b8035782
typhain <typhaine.charrat@gmail.com>
2019-08-12 16:31:55 +0200

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
+
+  after_create :welcome_send
+
   has_many :attendances
   has_many :events, through: :attendances
   has_many :event_admin, foreign_key: "administrator_id", class_name: "Event"
+
+
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
+
 20.times do
-  User.create(description: Faker::Lorem.sentence, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  User.create(email: "#{Faker::Lorem.word}@yopmail.com", description: Faker::Lorem.sentence, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 end
+
+10.times do
+  Event.create(start_date: Faker::Date.forward(days: 120), duration: rand(60..1440), title: Faker::Lorem.sentence(word_count: 3), description: Faker::Lorem.sentence, price: rand(1..100), location: Faker::Address.full_address, administrator: User.all.sample)
+end
+puts "Created 10 fake events."
+
+10.times do
+  Attendance.create(user: User.all.sample, event: Event.all.sample)
+end
+
+puts "Created 10 fake attendances."
