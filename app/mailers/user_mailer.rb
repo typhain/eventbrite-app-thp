e981539fa93e164f71a9c81c381db3efc50b83de
typhain <typhaine.charrat@gmail.com>
2019-08-13 10:40:04 +0200

    devise

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -11,4 +11,18 @@ class UserMailer < ApplicationMailer
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
+
+  def send_email(event, attendance)
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+
+    @event = event
+    @admin = event.administrator_id
+    @attendance = attendance
+    #on définit une variable @url qu'on utilisera dans la view d’e-mail
+    @url  = 'http://monsite.fr/login'
+
+    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+    mail(to: @admin_email, subject: 'Tu as un participant à ton événement !')
+  end
+
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,12 @@
 class Attendance < ApplicationRecord
+
+  after_create :send_email
+
   belongs_to :user
   belongs_to :event
+
+  def send_email
+    AdminMailer.send_email(self).deliver_now
+  end
+
 end
