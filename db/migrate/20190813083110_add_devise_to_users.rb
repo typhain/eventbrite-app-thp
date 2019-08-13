f5347f7d39957a43ba2f72e50a564bc4d4a8a57f
typhain <typhaine.charrat@gmail.com>
2019-08-13 11:03:57 +0200

    devise_update

--- db/migrate/20190813083110_add_devise_to_users.rb
+++ db/migrate/20190813083110_add_devise_to_users.rb
@@ -3,9 +3,6 @@
 class AddDeviseToUsers < ActiveRecord::Migration[5.2]
   def self.up
     change_table :users do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
